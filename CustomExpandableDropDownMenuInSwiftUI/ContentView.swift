db3633a8cf1312694336f8928e07596c24771d63
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-30 16:31:33 +0200

    init

--- CustomExpandableDropDownMenuInSwiftUI/ContentView.swift
+++ CustomExpandableDropDownMenuInSwiftUI/ContentView.swift
@@ -10,12 +10,54 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        DropDown()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct DropDown: View {
+    @State var expand = false
+    var body: some View {
+        VStack(alignment: .leading, spacing: 18, content: {
+            HStack {
+                Text("Expand").fontWeight(.heavy).foregroundColor(.white)
+                Image(systemName: expand ? "chevron.up" : "chevron.down")
+                    .resizable()
+                    .frame(width: 13, height: 6)
+                    .foregroundColor(.white)
+            }.onTapGesture {
+                self.expand.toggle()
+            }
+            
+            if expand {
+                Button(action: {
+                    print("1")
+                    self.expand.toggle()
+                }) {
+                    Text("Select 1")
+                }.foregroundColor(.white)
+                Button(action: {
+                    print("2")
+                    self.expand.toggle()
+                }) {
+                    Text("Select 2")
+                }.foregroundColor(.white)
+                Button(action: {
+                    print("3")
+                    self.expand.toggle()
+                }) {
+                    Text("Select 3")
+                }.foregroundColor(.white)
+            }
+        })
+        .padding()
+        .background(LinearGradient(gradient: .init(colors: [.red, .orange]), startPoint: .top, endPoint: .bottom))
+        .cornerRadius(20)
+        .animation(.spring())
     }
 }
